2da0b1f3a6b6bec0f01c1627052c25872b198730
Víctor J. Goyzueta <167643295+victor-goyzueta@users.noreply.github.com>
2026-01-28 15:59:40 +0100

    update ex02

---
+++ cpp-00/ex02/Account.cpp
@@ -0,0 +1,68 @@
+#include "Account.hpp"
+
+Account::Account() {}
+
+Account::Account(int initial_deposit)
+{
+	//
+}
+
+Account::~Account() 
+{
+	//
+}
+
+void	Account::_displayTimestamp()
+{
+	//
+}
+
+int	Account::getNbAccounts()
+{
+	//
+}
+
+int	Account::getTotalAmount( void )
+{
+	//
+}
+
+int	Account::getTotalAmount()
+{
+	//
+}
+
+int	Account::getNbDeposits()
+{
+	//
+}
+
+int	Account::getNbWithdrawals()
+{
+	//
+}
+
+void	Account::displayAccountsInfos()
+{
+	//
+}
+
+void	Account::makeDeposit(int deposit)
+{
+	//
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	//
+}
+
+int	Account::checkAmount() const
+{
+	//
+}
+
+void	Account::displayStatus() const
+{
+	//
+}
