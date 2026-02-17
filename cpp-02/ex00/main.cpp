52dc1c2037ecbe7eccff468b812ce623712fba01
Víctor J. Goyzueta <167643295+victor-goyzueta@users.noreply.github.com>
2026-02-17 19:32:55 +0100

    update

--- cpp-02/ex00/main.cpp
+++ cpp-02/ex00/main.cpp
@@ -1,6 +1,18 @@
 #include "inc/Fixed.hpp"
 
+#include <iostream>
+
 int	main(void)
 {
-	/*here*/
+	Fixed	a;
+	Fixed	b( a );
+	Fixed	c;
+
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
+	return (0);
 }
