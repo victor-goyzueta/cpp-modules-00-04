270611e2cc8eaf920c9162327a4a3c75edf58502
Víctor J. Goyzueta <167643295+victor-goyzueta@users.noreply.github.com>
2026-04-15 01:22:55 +0200

    update cpp03/ex00

--- cpp-03/ex00/main.cpp
+++ cpp-03/ex00/main.cpp
@@ -5,13 +5,18 @@
 int	main()
 {
 	ClapTrap	Bob("Bob");
+	ClapTrap	noName;
+	ClapTrap	Clone(Bob);
 
 	Bob.getStatus();
+
 	Bob.attack("Enemy");
 	Bob.takeDamage(1);
 	Bob.beRepaired(2);
 
 	Bob.getStatus();
+	noName.getStatus();
+	Clone.getStatus();
 
 	return(0);
 }
