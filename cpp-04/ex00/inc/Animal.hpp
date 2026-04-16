9e24a9b04bfaee6c349801c791cc7e0b723ce8ae
Víctor J. Goyzueta <167643295+victor-goyzueta@users.noreply.github.com>
2026-04-16 23:15:15 +0200

    cpp04

---
+++ cpp-04/ex00/inc/Animal.hpp
@@ -0,0 +1,21 @@
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+# include <iostream>
+
+class Animal
+{
+	protected:
+		std::string	_type;
+
+	public:
+		Animal();
+		Animal( const Animal& copy );
+		~Animal();
+
+		Animal& operator=( const Animal& copy );
+
+		virtual void	makeSound();
+};
+
+#endif
